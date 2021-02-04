0a1c5beb3b79851eeb68a850e693b9e89f572971
孙一昊 <18645574897@163.com>
2021-02-05 00:06:28 +0800

    模糊查询

--- src/views/Article/ArticleEditor.js
+++ src/views/Article/ArticleEditor.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Form,Input,Button,message,Select } from 'antd'
+import { Form, Input, Button, message, Select } from 'antd'
 import Editor from '@/components/MdEditor'
 import SimpleMDE from "react-simplemde-editor";
-import {addArticle,getActicleDetail,updateActicle} from '@/api/acticle'
+import { addArticle, getActicleDetail, updateActicle } from '@/api/acticle'
 import { getAllTag } from '@/api/tag'
 import showdown from 'showdown'
 import MdEditor from 'react-markdown-editor-lite'
@@ -16,115 +16,22 @@ import utils from 'markdown-it/lib/common/utils';
 const formTailLayout = {
     labelCol: { span: 3 },
     wrapperCol: { span: 18, offset: 1 },
-  };
+};
 
 const { TextArea } = Input
 const { Option } = Select
-class ArticleAdd extends React.Component  {
-    constructor() {
-        super()
-        this.state = {
-            textValue: 23,
-            act_detail:'',
-            ins:null,
-            tagList: []
-        }
-        this.mdParser = new MarkdownIt(/* Markdown-it options */)
+@Form.create()
+class ArticleAdd extends React.Component {
+    mdParser = new MarkdownIt(/* Markdown-it options */)
+    state = {
+        textValue: 23,
+        act_detail: '',
+        ins: null,
+        tagList: []
     }
     componentDidMount() {
         this.getEditorData()
         this.getAllTag()
-    }
-    render() {
-        const toolbar = [
-            'bold',
-            'italic',
-            'heading',
-            '|',
-            'quote',
-            'code',
-            'table',
-            'horizontal-rule',
-            'unordered-list',
-            'ordered-list',
-            '|',
-            'link',
-            'image',
-            '|',
-            'side-by-side',
-            'fullscreen',
-        ]
-        const { getFieldDecorator } = this.props.form
-        let { act_detail,tagList } = this.state
-        return <div className="wrapper form-content">
-            <Form {...formTailLayout}  size="middle">
-                <Form.Item  name="act_title" label="文章标题">
-                    {
-                        getFieldDecorator('act_title',{
-                            rules: [
-                                { required: true, message:'请填写标题' }
-                            ]
-                        })(
-                            <Input style={{width:WIDTH}}></Input>
-                        )
-                    }
-                </Form.Item>
-                <Form.Item name="act_title" label="文章标题">
-                    {
-                        getFieldDecorator('tags')(
-                            <Select style={{width:WIDTH}} mode="multiple">
-                               { tagList.map(item=>{
-                                   return <Option key={item.tag_id}  value={item.tag_id}>{item.tag_name}</Option>
-                               })}
-                            </Select>
-                        )
-                    }
-                </Form.Item>
-                <Form.Item name="main_content" label="文章梗概">
-                    {
-                        getFieldDecorator('main_content',{
-                            rules: [
-                                { required: true, message:'请填写梗概' }
-                            ]
-                        })(
-                            <TextArea style={{width:WIDTH}} rows={4}></TextArea>
-                        )
-                    }
-                </Form.Item>
-
-                <Form.Item name="act_detail" label="文章内容">
-                {
-                        getFieldDecorator('act_detail',{
-                            rules: [
-                                { required: true, message:'请填写文章内容' }
-                            ]
-                        })(
-                            <Editor text={act_detail} onChange={this.handleEditorChange}></Editor>
-                        )
-                    }
-                    {/* <SimpleMDE onChange={this.handleChange} />; */}
-                    {/* <SimpleMDE
-                            id="your-custom-id"
-                            getMdeInstance= { this.getInsance }
-                            onChange={this.handleChange}
-                            // toolbar={toolbar}
-                            // options={{
-                            //     toolbar
-                            // }}
-                        /> */}
-                        {/* <MdEditor
-                            value={act_detail}
-                            style={{height:400}}
-                            renderHTML={(text) => this.mdParser.render(text)}
-                            onChange={this.handleEditorChange} 
-                        />  */}
-                </Form.Item>
-                <Form.Item wrapperCol={{ span: 12, offset: 5 }}>
-                    <Button className="m-right20" onClick={this.save} type="primary" htmlType="submit" >保存</Button>
-                    <Button onClick={this.back} type="primary" htmlType="submit" >返回</Button>
-                </Form.Item>
-            </Form>
-        </div>
     }
     back = () => {
         this.props.history.goBack()
@@ -132,7 +39,7 @@ class ArticleAdd extends React.Component  {
     save = () => {
         const query = getQueryObj(this.props.location.search)
         const { id } = query
-        this.props.form.validateFields().then(res=>{
+        this.props.form.validateFields().then(res => {
             // const converter = new showdown.Converter()
             const act_detail = this.state.act_detail
             const tags = res.tags.join(',')
@@ -141,32 +48,32 @@ class ArticleAdd extends React.Component  {
                 act_detail,
                 tags
             }
-            id ? 
-            this.updateActicle({...res,act_detail,id}) : 
-            this.addArticle({...res,act_detail})
-        }).catch(error=>{
+            id ?
+                this.updateActicle({ ...res, act_detail, id }) :
+                this.addArticle({ ...res, act_detail })
+        }).catch(error => {
             console.log(error)
         })
     }
     updateActicle(parmars) {
-        updateActicle(parmars).then(response=>{
+        updateActicle(parmars).then(response => {
             if (response.res) {
                 message.success(response.msg)
                 this.props.history.goBack()
             }
         })
     }
-    handleEditorChange = (e) => {    
+    handleEditorChange = (e) => {
         console.log('handleEditorChange', e)
         this.props.form.setFieldsValue({
-            act_detail:e.text
+            act_detail: e.text
         })
         this.setState({
-            act_detail:e.text
+            act_detail: e.text
         })
     }
     addArticle(parmars) {
-        addArticle(parmars).then(response=>{
+        addArticle(parmars).then(response => {
             console.log(response)
             if (response.res) {
                 message.success(response.msg)
@@ -176,15 +83,15 @@ class ArticleAdd extends React.Component  {
     }
 
     handleChange = (e) => {
-        this.setState({act_detail:e})
+        this.setState({ act_detail: e })
     }
     getInsance = (ins) => {
-        this.setState({ins})
+        this.setState({ ins })
     }
     getAllTag = () => {
-        getAllTag().then(res=>{
+        getAllTag().then(res => {
             console.log(res)
-            if(res.status === 200) {
+            if (res.status === 200) {
                 this.setState({
                     tagList: res.data.rows
                 })
@@ -194,22 +101,80 @@ class ArticleAdd extends React.Component  {
     getEditorData() {
         const query = getQueryObj(this.props.location.search)
         const { id } = query
-        if(!id) {
+        if (!id) {
             return
         }
-        getActicleDetail({id}).then(res=>{
+        getActicleDetail({ id }).then(res => {
             if (res.res) {
                 const data = {
                     ...res.data,
-                    tags:res.data.tags.split(',')
+                    tags: res.data.tags ? res.data.tags.split(',') : []
                 }
                 this.props.form.setFieldsValue(data)
-                this.setState({act_detail:res.data.act_detail})
+                this.setState({ act_detail: res.data.act_detail })
             } else {
                 message.error(res.message)
             }
         })
     }
+
+    render() {
+        const { getFieldDecorator } = this.props.form
+        let { act_detail, tagList } = this.state
+        return <div className="wrapper form-content">
+            <Form {...formTailLayout} size="middle">
+                <Form.Item name="act_title" label="文章标题">
+                    {
+                        getFieldDecorator('act_title', {
+                            rules: [
+                                { required: true, message: '请填写标题' }
+                            ]
+                        })(
+                            <Input style={{ width: WIDTH }}></Input>
+                        )
+                    }
+                </Form.Item>
+                <Form.Item name="act_title" label="文章标题">
+                    {
+                        getFieldDecorator('tags')(
+                            <Select style={{ width: WIDTH }} mode="multiple">
+                                {tagList.map(item => {
+                                    return <Option key={item.tag_id} value={item.tag_id}>{item.tag_name}</Option>
+                                })}
+                            </Select>
+                        )
+                    }
+                </Form.Item>
+                <Form.Item name="main_content" label="文章梗概">
+                    {
+                        getFieldDecorator('main_content', {
+                            rules: [
+                                { required: true, message: '请填写梗概' }
+                            ]
+                        })(
+                            <TextArea style={{ width: WIDTH }} rows={4}></TextArea>
+                        )
+                    }
+                </Form.Item>
+
+                <Form.Item name="act_detail" label="文章内容">
+                    {
+                        getFieldDecorator('act_detail', {
+                            rules: [
+                                { required: true, message: '请填写文章内容' }
+                            ]
+                        })(
+                            <Editor text={act_detail} onChange={this.handleEditorChange}></Editor>
+                        )
+                    }
+                </Form.Item>
+                <Form.Item wrapperCol={{ span: 12, offset: 5 }}>
+                    <Button className="m-right20" onClick={this.save} type="primary" htmlType="submit" >保存</Button>
+                    <Button onClick={this.back} type="primary" htmlType="submit" >返回</Button>
+                </Form.Item>
+            </Form>
+        </div>
+    }
 }
 
-export default Form.create({ name:'article_add' })(ArticleAdd)
+export default ArticleAdd
--- src/views/Article/Articlelist.js
+++ src/views/Article/Articlelist.js
@@ -1,120 +1,70 @@
 import React from 'react'
 import './Articlelist.scss'
-import { Table,Row,Col,Input,Button,Modal,message } from 'antd'
-import {getActicleList,delActicle} from '@/api/acticle'
+import { Table, Row, Col, Input, Button, Modal, message, Form } from 'antd'
+import { getActicleList, delActicle } from '@/api/acticle'
 class Home extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      dataSource: [],
-      columns: [
-        {
-          title: '标题',
-          dataIndex: 'act_title',
-          key: 1
-        },{
-          title: '梗概',
-          dataIndex: 'main_content',
-          key: 2
-        },{
-          title: '喜欢',
-          dataIndex: 'likes',
-          key: 6
-        },{
-          title: '创建时间',
-          dataIndex: 'create_time',
-          key: 3
-        },{
-          title: '更新时间',
-          dataIndex: 'update_time',
-          key: 4
-        },{
-          title:'操作',
-          key:7,
-          render:(item) => {
-            return <div>
-              <span onClick={()=>this.updateArticle(item)} className="primary">修改</span>
-              <span onClick={()=>this.delArticle(item)} className="danger">删除</span>
-            </div>
-          }
+  state = {
+    dataSource: [],
+    columns: [
+      {
+        title: '标题',
+        dataIndex: 'act_title',
+        key: 1
+      }, {
+        title: '梗概',
+        dataIndex: 'main_content',
+        key: 2
+      }, {
+        title: '喜欢',
+        dataIndex: 'likes',
+        key: 6
+      }, {
+        title: '创建时间',
+        dataIndex: 'create_time',
+        key: 3
+      }, {
+        title: '更新时间',
+        dataIndex: 'update_time',
+        key: 4
+      }, {
+        title: '操作',
+        key: 7,
+        render: (item) => {
+          return <div>
+            <span onClick={() => this.updateArticle(item)} className="primary">修改</span>
+            <span onClick={() => this.delArticle(item)} className="danger">删除</span>
+          </div>
         }
-      ],
-      pagination:{
-        current: 1,
-        pageSize: 10,
-        total: 20
-      },
-    }
+      }
+    ],
+    pagination: {
+      current: 1,
+      pageSize: 10,
+      total: 20
+    },
   }
   componentDidMount() {
     this.getActicleList()
   }
-  render () {
-    const {columns,dataSource,pagination} = this.state
-    return <div className="articlelist-container router-container">
-      <div className="query-form">
-        <Row key="23" gutter={10}>
-          <Col span={4}>
-            <Input placeholder="文章标题"></Input>
-          </Col>
-          <Col span={4}>
-            <Input placeholder="文章标题"></Input>
-          </Col>
-          <Col span={4}>
-            <Input placeholder="文章标题"></Input>
-          </Col>
-          <Col span={4}>
-            <Input placeholder="文章标题"></Input>
-          </Col>
-          <Col span={4}>
-            <Input placeholder="文章标题"></Input>
-          </Col>
-          <Col span={4}>
-            <Button className="m-right20" type="primary">查询</Button>
-            <Button type="danger">重置</Button>
-          </Col>
-        </Row>
-        <div className="btn-group">
-          <Button onClick={this.addArticle} type="primary">新增</Button>
-        </div>
-        <Table bordered onChange={this.tableChange} pagination={pagination} columns={columns} dataSource={dataSource}></Table>
-      </div>
-    </div>
-  }
-  delArticle = ({id}) => {
-    // delActicle
-    var _t = this
-    Modal.confirm({
-      title:'提示',
-      content:'是否确定删除?',
-      onOk() {
-        delActicle({id}).then(response=>{
-          if(response.res) {
-            message.success(response.msg)
-            _t.getActicleList()
-          }
-        })
-      }
-    })
-  }
+
   tableChange = (pagination) => {
     console.log(pagination)
     this.setState({
-      pagination:{
+      pagination: {
         ...pagination
       }
-    },()=>{
+    }, () => {
       this.getActicleList()
     })
   }
   getActicleList = () => {
-    const {current,pageSize} = this.state.pagination
-    getActicleList({page:current-1,size:pageSize}).then(res=>{
+    const { pagination: { current, pageSize }, act_title } = this.state
+    getActicleList({ page: current - 1, size: pageSize, act_title }).then(res => {
       console.log(res)
       if (res.res) {
         this.setState({
-          dataSource: res.data.rows.map(item=>({...item,key:item.id})),
-          pagination:{...this.state.pagination,total:res.data.total}
+          dataSource: res.data.rows.map(item => ({ ...item, key: item.id })),
+          pagination: { ...this.state.pagination, total: res.data.total }
         })
       }
     })
@@ -125,7 +75,53 @@ class Home extends React.Component {
 
   updateArticle = (item) => {
     const { id } = item
-    this.props.history.push('/article/Articleedit?id='+id)
+    this.props.history.push('/article/Articleedit?id=' + id)
+  }
+  delArticle = ({ id }) => {
+    var _t = this
+    Modal.confirm({
+      title: '提示',
+      content: '是否确定删除?',
+      onOk() {
+        delActicle({ id }).then(response => {
+          if (response.res) {
+            message.success(response.msg)
+            _t.getActicleList()
+          }
+        })
+      }
+    })
+  }
+
+  render() {
+    const { columns, dataSource, pagination, act_title } = this.state
+    return <div className="articlelist-container router-container">
+      <div className="query-form">
+        <Row key="23" gutter={10}>
+          <Col span={4}>
+            <Input value={act_title} onChange={(e) => {
+              this.setState({
+                act_title: e.target.value
+              })
+            }} placeholder="文章标题"></Input>
+          </Col>
+          <Col span={4}>
+            <Button onClick={this.getActicleList} className="m-right20" type="primary">查询</Button>
+            <Button onClick={() => {
+              this.setState({
+                act_title: ''
+              }, () => {
+                this.getActicleList()
+              })
+            }} type="danger">重置</Button>
+          </Col>
+        </Row>
+        <div className="btn-group">
+          <Button onClick={this.addArticle} type="primary">新增</Button>
+        </div>
+        <Table bordered onChange={this.tableChange} pagination={pagination} columns={columns} dataSource={dataSource}></Table>
+      </div>
+    </div>
   }
 }
 
